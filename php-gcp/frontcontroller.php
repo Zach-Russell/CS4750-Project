d811481fa68d72da6b8320f7fb4fef9ce6568214
Sara Lewis <shl5tnd@virginia.edu>
2023-12-03 23:46:46 -0500

    frontcontroller

--- php-gcp/frontcontroller.php
+++ php-gcp/frontcontroller.php
@@ -6,45 +6,45 @@
  * for more examples.
  */
 # [START gae_simple_front_controller]
-switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
-    case '/index.php':
-        require 'index.php';
-        break;
-    case '/login.php':
-        require 'login.php';
-        break;
-    case '/dbconnection.php':
-        require 'dbconnection.php';
-        break;
-    case '/logout.php':
-        require 'logout.php';
-        break;
-    case '/groceryItems.php':
-        require 'groceryItems.php';
-        break;
-    case '/myLists.php':
-        require 'myLists.php';
-        break;
-    case '/register.php':
-        require 'register.php';
-        break;
-    case '/search.php':
-        require 'search.php';
-        break;
-    case '/searchCategory.php':
-        require 'searchCategory.php';
-        break;
-    case '/settings.php':
-        require 'settings.php';
-        break;
-    case '/':
-        require 'index.php';
-        break;
-    default:
-        require 'index.php';
-        break;
+// switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+//     case '/index.php':
+//         require 'index.php';
+//         break;
+//     case '/login.php':
+//         require 'login.php';
+//         break;
+//     case '/dbconnection.php':
+//         require 'dbconnection.php';
+//         break;
+//     case '/logout.php':
+//         require 'logout.php';
+//         break;
+//     case '/groceryItems.php':
+//         require 'groceryItems.php';
+//         break;
+//     case '/myLists.php':
+//         require 'myLists.php';
+//         break;
+//     case '/register.php':
+//         require 'register.php';
+//         break;
+//     case '/search.php':
+//         require 'search.php';
+//         break;
+//     case '/searchCategory.php':
+//         require 'searchCategory.php';
+//         break;
+//     case '/settings.php':
+//         require 'settings.php';
+//         break;
+//     case '/':
+//         require 'index.php';
+//         break;
+//     default:
+//         require 'index.php';
+//         break;
 
-}
+// }
 
 
 
